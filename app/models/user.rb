8fd9415b557d8e838135b6028abd458369d73c3b
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-12 10:03:23 -0600

    implement feature: user creates an account - create user model with validations - create routes, controllers and views for the root page and page to land on after creating an account

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+  validates_confirmation_of :password
+end
---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.feature "visitor creates an account" do
+  context "successfully" do
+    scenario "as a new user" do
+      email = "erin@turing.io"
+      password = "password"
+      password_confirmation = "password"
+
+      expect(User.count).to eq 0
+
+      visit '/'
+      click_link "Sign Up"
+      fill_in "user[email]",    with: email
+      fill_in "user[password]", with: password
+      fill_in "user[password_confirmation]", with: password_confirmation
+      click_button 'Sign Up'
+
+      expect(current_path).to eq links_path
+      expect(User.count).to eq 1
+      expect(page).not_to have_content "Log In"
+      # expect(page).to have_content "Log Out"
+    end
+  end
+end
