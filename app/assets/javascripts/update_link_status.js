86be58cdb09313d69750f9e7c716b357a00a521a
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-12 15:19:50 -0600

    add js to change the status of a link upon click of mark read/unread buttons - updates are persisting to the db but are not reflected correctly in the css upon page reload, because the classes are not correctly written to reflect the current status

---
+++ app/assets/javascripts/update_link_status.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  function updateStatus(currentStatus){
+    if (currentStatus === false) {
+      return "true"
+    } else {
+      return "false"
+    }
+  }
+
+  $(".all-links").on("click", ".mark-read", function(){
+    var currentStatus = $(this).parent().data("status");
+    var linkId = $(this).data("id");
+    var newStatus = updateStatus(currentStatus);
+    $.ajax({
+      url: "/api/v1/links/" + linkId,
+      method: "PATCH",
+      dataType: "JSON",
+      data: { link: { status: newStatus}},
+      success: function(idea){
+        $("#" + linkId).addClass("read")
+      },
+      error: function(){
+        console.log("Update status did not work")
+      }
+    })
+  })
+
+  $(".all-links").on("click", ".mark-unread", function(){
+    var currentStatus = $(this).parent().data("status");
+    var linkId = $(this).data("id");
+    var newStatus = updateStatus(currentStatus);
+    $.ajax({
+      url: "/api/v1/links/" + linkId,
+      method: "PATCH",
+      dataType: "JSON",
+      data: { link: { status: newStatus}},
+      success: function(idea){
+        $("#" + linkId).removeClass("read")
+      },
+      error: function(){
+        console.log("Update status did not work")
+      }
+    })
+  })
+})
