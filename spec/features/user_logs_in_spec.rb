9e71b93d09b052fc40e04e9e6ccb90a0eef19b81
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-20 10:19:37 -0600

    remove commented out code in login spec for sad path; will need to reimplement later

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -17,34 +17,4 @@ RSpec.feature "user logs in" do
       expect(page).to have_content("Log Out")
     end
   end
-
-  # context "with invalid information" do
-  #   scenario "with the wrong password" do
-  #     user = User.create(email: "erin@gmail.com", password: "password")
-  #     visit "/"
-  #     click_link "Log In"
-  #
-  #     fill_in "session[email]",    with: user.email
-  #     fill_in "session[password]", with: "WRONG PASSWORD"
-  #     fill_in "session[password_confirmation]", with: "WRONG PASSWORD"
-  #     click_button 'Log In'
-  #     save_and_open_page
-  #     expect(page).to have_content "Invalid Password"
-  #     expect(current_path).to eq(login_path)
-  #   end
-  #
-  #   scenario "with an email that hasn't been registered" do
-  #     user = User.create(email: "tom@gmail.com", password: "password")
-  #     visit "/"
-  #     click_link "Log In"
-  #
-  #     fill_in "session[email]",    with: "nothing@gmail.com"
-  #     fill_in "session[password]", with: user.password
-  #     fill_in "session[password_confirmation]", with: user.password
-  #     click_button 'Log In'
-  #
-  #     expect(page).to have_content "Invalid Email"
-  #     expect(current_path).to eq(login_path)
-  #   end
-  # end
 end
