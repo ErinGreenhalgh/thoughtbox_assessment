f3e3be2eb199de9c6fb07e2333898b0bec5bdec9
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-20 14:25:52 -0600

    add assertions in updating link status feature spec about the background color of the div - these are passing right now - need to correct logic of having the right buttons show up depending on status - could also add assertions about changing status back to read once it has been unread and/or adding assertions about color and buttons once you refresh the page

--- spec/features/updating_link_status_spec.rb
+++ spec/features/updating_link_status_spec.rb
@@ -14,13 +14,27 @@ RSpec.feature "updating link status" do
       expect(page).not_to have_content("Mark as Read")
       expect(page).to have_content("Mark as Unread")
 
-      #expect background color of the div to be grey
+      link_div = find("div[id='#{link.id}']")
+      grey = "rgba(128, 128, 128, 1)"
+      expect(link_div.native.style("background-color")).to eql(grey)
     end
   end
 
   context "from read to unread", js: true do
     scenario "by clicking mark as unread" do
-      # expect background color of the div to be white
+      user = create(:user)
+      link = create(:link, user: user, status: "read")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit user_links_path(user)
+      expect(page).not_to have_content("Mark as Read")
+      expect(page).to have_content("Mark as Unread")
+      click_button "Mark as Unread"
+      expect(page).to have_content("Mark as Read")
+      expect(page).not_to have_content("Mark as Unread")
+
+      link_div = find("div[id='#{link.id}']")
+      expect(link_div.native.style("background-color")).to eql("transparent")
     end
   end
 end
